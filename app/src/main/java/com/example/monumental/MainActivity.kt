64b8d3f2019a84c225de4452e961377db38b996b
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2020-08-23 17:04:41 +0200

    Added splash screen

--- app/src/main/java/com/example/monumental/MainActivity.kt
+++ app/src/main/java/com/example/monumental/MainActivity.kt
@@ -69,6 +69,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setTheme(R.style.AppTheme)
 
         requestPermissions()
 
