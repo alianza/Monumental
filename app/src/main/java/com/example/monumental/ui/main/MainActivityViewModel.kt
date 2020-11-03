73f3b14916c4f17f7034fbe4da3c6d8a32c327a1
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2020-11-03 01:03:32 +0100

    Added MainViewModel

---
+++ app/src/main/java/com/example/monumental/ui/main/MainActivityViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.monumental.ui.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.monumental.model.Landmark
+import com.example.monumental.room.repository.JourneyRepository
+import com.example.monumental.room.repository.LandmarkRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val landmarkRepository = LandmarkRepository(application.applicationContext)
+    private val journeyRepository = JourneyRepository(application.applicationContext)
+
+    private val mainScope = CoroutineScope(Dispatchers.Main)
+
+    var activeJourney = journeyRepository.getActiveJourney()
+
+    fun createLandmark(landmark: Landmark) {
+        mainScope.launch {
+            landmarkRepository.insertLandmark(landmark)
+        }
+    }
+}
