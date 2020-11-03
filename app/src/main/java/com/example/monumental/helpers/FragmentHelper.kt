52c93bebb82cad6557fed1c0505549f03f111db7
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2020-11-03 22:36:10 +0100

    Added fragment transitions

--- app/src/main/java/com/example/monumental/helpers/FragmentHelper.kt
+++ app/src/main/java/com/example/monumental/helpers/FragmentHelper.kt
@@ -2,6 +2,7 @@ package com.example.monumental.helpers
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentTransaction.*
 import com.example.monumental.R
 import com.example.monumental.model.Journey
 import com.example.monumental.ui.journey.JourneyFragment
@@ -17,16 +18,18 @@ class FragmentHelper(private val context: AppCompatActivity) {
 
     fun openJourneyFragment() {
         context.supportFragmentManager.beginTransaction()
+            .setTransition(TRANSIT_FRAGMENT_OPEN)
             .replace(R.id.journey_fragment_container, journeyFragment)
-            .commitNow()
+            .commitAllowingStateLoss()
         journeyFragmentIsOpen = true
     }
 
     fun closeJourneyFragment(): Boolean {
         if (journeyFragmentIsOpen) {
             context.supportFragmentManager.beginTransaction()
+                .setTransition(TRANSIT_FRAGMENT_FADE)
                 .remove(journeyFragment)
-                .commitNow()
+                .commitAllowingStateLoss()
             this.journeyFragmentIsOpen = false
             return true
         }
@@ -39,16 +42,18 @@ class FragmentHelper(private val context: AppCompatActivity) {
 
         landmarkFragment.arguments = arguments
         context.supportFragmentManager.beginTransaction()
+            .setTransition(TRANSIT_FRAGMENT_OPEN)
             .replace(R.id.landmark_fragment_container, landmarkFragment)
-            .commitNow()
+            .commitAllowingStateLoss()
         landmarkFragmentIsOpen = true
     }
 
     fun closeLandmarkFragment(): Boolean {
         if (landmarkFragmentIsOpen) {
             context.supportFragmentManager.beginTransaction()
+                .setTransition(TRANSIT_FRAGMENT_CLOSE)
                 .remove(landmarkFragment)
-                .commitNow()
+                .commitAllowingStateLoss()
             landmarkFragmentIsOpen = false
             landmarkFragment = LandmarkFragment.newInstance() // Refresh instance
             return true
