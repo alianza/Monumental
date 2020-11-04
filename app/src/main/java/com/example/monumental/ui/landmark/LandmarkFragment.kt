0dd7ff970c663e788c3bdc45b0867691000a35d3
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2020-11-04 21:17:57 +0100

    Added date in dialog

--- app/src/main/java/com/example/monumental/ui/landmark/LandmarkFragment.kt
+++ app/src/main/java/com/example/monumental/ui/landmark/LandmarkFragment.kt
@@ -6,15 +6,23 @@ import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.bogdwellers.pinchtozoom.ImageMatrixTouchHandler
 import com.example.monumental.R
 import com.example.monumental.model.Journey
 import com.example.monumental.model.Landmark
 import com.example.monumental.ui.main.MainActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.landmark_fragment.*
+import java.io.File
+import java.net.URI
+
 
 class LandmarkFragment : Fragment() {
 
@@ -28,6 +36,9 @@ class LandmarkFragment : Fragment() {
 
     private lateinit var landmarkAdapter: LandmarkAdapter
     private lateinit var journey: Journey
+
+    private val MONTH_OFFSET = 1
+    private val YEAR_OFFSET = 1900
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,12 +68,10 @@ class LandmarkFragment : Fragment() {
         rvLandmarks.adapter = landmarkAdapter
 
         viewModel.getLandmarksByJourney(journey.id!!).observe(viewLifecycleOwner, { landmarks ->
-            println("Observe! " + landmarks)
             if (landmarks != null) {
                 this.landmarks.clear()
                 landmarks.forEach { landmark ->
                     this.landmarks.add(landmark)
-                    println("Got: $landmark")
                 }
             }
             //            this.landmarks.sortByDescending { it.name }
@@ -88,16 +97,20 @@ class LandmarkFragment : Fragment() {
     private fun setListeners() {
         btnClose.setOnClickListener { closeFragment() }
         tvBack.setOnClickListener { closeFragment() }
-        swCurrentJourney.setOnCheckedChangeListener { buttonView, isChecked ->
+        swCurrentJourney.setOnCheckedChangeListener { _, isChecked ->
             toggleActiveJourney(isChecked)
         }
     }
 
     private fun toggleActiveJourney(checked: Boolean) {
-        println("Checked! " + checked)
         if (checked) {
             journey.current = swCurrentJourney.isChecked
             viewModel.setActiveJourney(journey)
+            Toast.makeText(
+                context,
+                getString(R.string.set_current_journey, journey.name),
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 
@@ -110,8 +123,9 @@ class LandmarkFragment : Fragment() {
 
         builder.setPositiveButton("Yes") { dialog, _ ->
             Handler().postDelayed({
-            viewModel.deleteLandmark(landmark)
-            dialog.dismiss() }, actionDelayVal) }
+                viewModel.deleteLandmark(landmark)
+                dialog.dismiss()
+            }, actionDelayVal) }
 
         builder.setNegativeButton("No", null)
 
@@ -121,8 +135,36 @@ class LandmarkFragment : Fragment() {
         }, actionDelayVal)
     }
 
-    private fun landmarkClick(landmark: Landmark) {
 
+
+    private fun landmarkClick(landmark: Landmark) {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(context)
+
+        val title = TextView(context)
+        title.setText(landmark.name + " " + getString(R.string.date_format,
+            landmark.date?.date.toString(),
+            (landmark.date?.month?.plus(MONTH_OFFSET)).toString(),
+            (landmark.date?.year?.plus(YEAR_OFFSET)).toString()))
+        title.setPadding(20, 20, 20, 20)
+        title.textSize = 20f
+        title.setTextColor(resources.getColor(R.color.design_default_color_on_secondary))
+        builder.setCustomTitle(title)
+
+        builder.setNegativeButton("Close", null)
+
+        val dialogView = layoutInflater.inflate(R.layout.dialog_landmark_view, null)
+
+        val imageView = dialogView.findViewById<ImageView>(R.id.ivLandmark).also {
+            it.setOnTouchListener(ImageMatrixTouchHandler(view?.context))
+        }
+
+        println("Landmark " + landmark.img_uri)
+
+        Picasso.get().load(File(URI.create(landmark.img_uri))).into(imageView)
+
+        builder.setView(dialogView)
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.show()
     }
 
     private fun closeFragment() {
@@ -131,5 +173,6 @@ class LandmarkFragment : Fragment() {
             (activity as MainActivity?)?.fragmentHelper?.closeLandmarkFragment()
         }, actionDelayVal)
     }
-
 }
+
+
